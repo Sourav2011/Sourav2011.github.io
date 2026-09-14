14373bfe8aeb72e0cd10b5faa5b7b1138a3cf668
Sourav Poddar <35718974+Sourav2011@users.noreply.github.com>
2026-09-14 19:39:52 +0200

    Animated Background

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ window.addEventListener("scroll", () => {
 btn.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+// MODAL POPUPS
+function openModal(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+window.onclick = function(event) {
+    document.querySelectorAll('.modal').forEach(modal => {
+        if (event.target === modal) modal.style.display = "none";
+    });
+};
